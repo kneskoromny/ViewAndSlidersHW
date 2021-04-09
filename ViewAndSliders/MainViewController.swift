461e4841433eb34e3cdc0f9b216689e2db7278e9
Kirill Neskoromny <kneskoromny@gmail.com>
2021-04-09 16:16:14 +0300

    finished main part of HW

--- ViewAndSliders/MainViewController.swift
+++ ViewAndSliders/MainViewController.swift
@@ -7,48 +7,49 @@
 
 import UIKit
 
-// протокол объявляется в том классе, где он будет реализован, куда будут передаваться данные
 protocol SettingsViewControllerDelegate {
-    func setNewColor(for redSliderValue: Float, and greenSliderValue: Float, and blueSliderValue: Float)
+    func setNewColor(
+        for redSliderValue: Float,
+        and greenSliderValue: Float,
+        and blueSliderValue: Float)
 }
 
 class MainViewController: UIViewController {
-
+    
     
     @IBOutlet var mainView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         mainView.backgroundColor = UIColor(
-            red: 255/255,
-            green: 255/255,
-            blue: 255/255,
+            red: 255,
+            green: 255,
+            blue: 255,
             alpha: 1)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let settingsVC = segue.destination as? SettingsViewController {
-            settingsVC.mainViewColor = view.backgroundColor
-            //инициализирую свойство классом MainViewController, тк он подписан на этот тип данных
+            settingsVC.mainViewColor = mainView.backgroundColor
             settingsVC.delegate = self
         }
     }
-
 }
-// здесь реализовали метод протокола, показали как использовать параметры, которые получим - передадим их в свойство UIColor
-// теперь класс имеет тип данных SettingsViewController
+
 
 extension MainViewController: SettingsViewControllerDelegate {
-    func setNewColor(for redSliderValue: Float, and greenSliderValue: Float, and blueSliderValue: Float) {
+    func setNewColor(
+        for redSliderValue: Float,
+        and greenSliderValue: Float,
+        and blueSliderValue: Float) {
+        
         mainView.backgroundColor = UIColor(
             red: CGFloat(redSliderValue),
             green: CGFloat(greenSliderValue),
             blue: CGFloat(blueSliderValue),
             alpha: 1)
     }
-    
-    
 }
 
 
--- ViewAndSliders/SettingsViewController.swift
+++ ViewAndSliders/SettingsViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
-    //MARK: - IB Outlets
     @IBOutlet weak var mainView: UIView!
     
     @IBOutlet weak var redSliderValueLabel: UILabel!
@@ -20,42 +19,25 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
-    // передаю цвет с главного экрана через сегвей
     var mainViewColor: UIColor!
-    // объявляю свойство делегатом протокола и инициализируем его,
-    // у него есть нужный метод setNewСolor, delegate теперь является ссылкой на класс MainViewController (на тот extension, где есть метод setNewColor)
-    var delegate: SettingsViewControllerDelegate! //протокол - тип данных, как и любой другой String, Int и тд
+
+    var delegate: SettingsViewControllerDelegate!
     
-    
-    
-    
-    
-    
-    //MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // View
-        
         mainView.layer.cornerRadius = mainView.frame.height / 7
-        // присваиваю аутлету mainView цвет, переданный по сегвею
         mainView.backgroundColor = mainViewColor
-        // создаю массив из значений RGBA текущего цвета View
-        let colorsValues = (mainViewColor.cgColor.components)!
+       
+        getColors()
         
-        
-        // Slider
-        
+        // Sliders
         redSlider.minimumTrackTintColor = .red
         greenSlider.minimumTrackTintColor = .green
         blueSlider.minimumTrackTintColor = .blue
-        // присваиваю значения текущих цветов для каждого слайдера
-        redSlider.value = Float(colorsValues[0])
-        greenSlider.value = Float(colorsValues[1])
-        blueSlider.value = Float(colorsValues[2])
         
-        //Label
-        
+        //Labels
         redSliderValueLabel.text = String(format: "%.2f", redSlider.value)
         greenSliderValueLabel.text = String(format: "%.2f", greenSlider.value)
         blueSliderValueLabel.text = String(format: "%.2f",  blueSlider.value)
@@ -63,7 +45,6 @@ class SettingsViewController: UIViewController {
         
     }
     
-    //MARK: - IB Actions
     @IBAction func redSliderChanged() {
         redSliderValueLabel.text = String(format: "%.2f", redSlider.value)
         changeViewBackgroundColor()
@@ -78,23 +59,29 @@ class SettingsViewController: UIViewController {
     }
   
     @IBAction func doneButtonPressed() {
-        // перед тем, как закрыть экран должны вызвать метод setNewColor,
-        // чтобы передать цвета,
+        
         delegate.setNewColor(
             for: redSlider.value,
             and: greenSlider.value,
             and: blueSlider.value)
-        //закрываем экран
+        
         dismiss(animated: true)
     }
     
-    //MARK: - Private Methods
     private func changeViewBackgroundColor() {
         mainView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1)
+    }
+    
+    private func getColors() {
+        let colorsValues = (mainViewColor.cgColor.components)!
+        
+        redSlider.value = Float(colorsValues[0])
+        greenSlider.value = Float(colorsValues[1])
+        blueSlider.value = Float(colorsValues[2])
     }
    
 }
